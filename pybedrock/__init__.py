816c484f8e07479db889e9b533e79e93a6f4e694
obscraft <123219490+obscraft23@users.noreply.github.com>
2023-02-05 01:09:56 +0900

    Update __init__.py

--- pybedrock/__init__.py
+++ pybedrock/__init__.py
@@ -1,7 +1,7 @@
 from pybedrock.main import *
 
 __copyright__    = 'Copyright (C) 2023 obscraft23'
-__version__      = '0.0.1'
+__version__      = '0.0.2'
 __author__       = 'obscraft23'
 __author_email__ = 'obscraft23@gmail.com'
 __url__          = 'https://github.com/obscraft23/pybedrock'
