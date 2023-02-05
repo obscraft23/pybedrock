db7281d5e4612ffec2e9c4e4fcfafe01ba9ec36b
obscraft23 <obscraft23@gmail.com>
2023-02-05 20:14:50 +0900

    modify pybedrocklib

--- pybedrock/libpybedrock.cpp
+++ pybedrock/libpybedrock.cpp
@@ -2,9 +2,3 @@
 
 #include <stdio.h>
 #include <cassert>
-
-leveldb::DB* db;
-leveldb::Options options;
-options.create_if_missing = true;
-leveldb::Status status = leveldb::DB::Open(options, "../../TbLUY1uSXQA=/db", &db);
-assert(status.ok());
