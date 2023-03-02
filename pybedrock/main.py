74310765a1e303dc6647e9590bee36f11ffd6186
obscraft23 <obscraft23@gmail.com>
2023-03-03 08:55:51 +0900

    add main.py

--- pybedrock/main.py
+++ pybedrock/main.py
@@ -1,15 +1,1 @@
-import pybedrock.mcberepair as mr
-import pybedrock.nbt as nbt
-
-
-class world():
-    def __init__(self,wdname):
-        self.wdname = wdname
-        self.keycontents = mr.listkeys(self.wdname)
-
-    def updatelistkeys(self):
-        self.keycontents = mr.listkeys(self.wdname)
-
-    def loadNBTdatafromkey(self,key):
-        nbtbuffer = mr.getNBTfromkey(self.wdname,key)
-        return nbt.loadNBT(nbtbuffer)
+from pybedrock.cmodules.lib.libleveldbhandler import *
