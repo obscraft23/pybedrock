4c80086b44f63a4e327fa3e351764e92e9443067
obscraft23 <123219490+obscraft23@users.noreply.github.com>
2023-03-24 18:41:41 +0900

    v0.04->v0.05

--- pybedrock/__init__.py
+++ pybedrock/__init__.py
@@ -1,8 +1,11 @@
 from pybedrock.main import *
-from pybedrock.cmodules.lib.libleveldbhandler import *
+try:
+  from pybedrock.cmodules.lib.libleveldbhandler import *
+except:
+  from pybedrock.cmodules.lib64.libleveldbhandler import *
 
 __copyright__    = 'Copyright (C) 2023 obscraft23'
-__version__      = '0.0.4'
+__version__      = '0.0.5'
 __author__       = 'obscraft23'
 __author_email__ = 'obscraft23@gmail.com'
 __url__          = 'https://github.com/obscraft23/pybedrock'
