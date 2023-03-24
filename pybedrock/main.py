a61798572f7be613d66488af9afa1ee056844a7f
obscraft23 <123219490+obscraft23@users.noreply.github.com>
2023-03-24 18:28:44 +0900

    fix an import bug for .whl

--- pybedrock/main.py
+++ pybedrock/main.py
@@ -1,1 +1,4 @@
-from pybedrock.cmodules.lib.libleveldbhandler import *
+try:
+  from pybedrock.cmodules.lib.libleveldbhandler import *
+except:
+  from pybedrock.cmodules.lib64.libleveldbhandler import *
