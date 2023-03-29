a738f697f355c178f746302b650df527a3752505
obscraft23 <obscraft23@gmail.com>
2023-03-29 23:20:39 +0900

    bug fix for Win

--- patch/zlib_compressor.cc
+++ patch/zlib_compressor.cc
@@ -5,8 +5,8 @@
 //#pragma comment(lib, "C:/vcpkg/installed/x64-windows/lib/zlib.lib" )
 
 #define ZLIB_WINAPI
-#pragma comment(lib,"C:/vcpkg/installed/x64-windows/lib/zlib.lib")
-#pragma comment(lib,"C:/vcpkg/installed/x64-windows/lib/zlibd.lib")
+//#pragma comment(lib,"C:/vcpkg/installed/x64-windows/lib/zlib.lib")
+//#pragma comment(lib,"C:/vcpkg/installed/x64-windows/lib/zlibd.lib")
 
 #include <zlib.h>
 #include <algorithm>
