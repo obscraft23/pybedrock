4b47362a72c8c9db3debdbabfe4fe07b1d5b80ca
obscraft23 <obscraft23@gmail.com>
2023-03-31 08:30:52 +0900

    test

--- pybedrock/clib/leveldbhandler.cpp
+++ pybedrock/clib/leveldbhandler.cpp
@@ -11,15 +11,15 @@
 #include "mcbekey.hpp"
 
 #ifdef _WIN64
-__declspec(dllexport) extern "C" PyObject* py_listkeys(PyObject* self, PyObject* args);
-__declspec(dllexport) extern "C" PyObject* py_loadbinary(PyObject* self, PyObject* args);
-__declspec(dllexport) extern "C" PyObject* py_writebinary(PyObject* self, PyObject* args);
-__declspec(dllexport) extern "C" PyObject* py_rmkey(PyObject* self, PyObject* args);
-__declspec(dllexport) extern "C" PyObject* py_readNBT(PyObject* self, PyObject* args);
-__declspec(dllexport) extern "C" PyObject* py_readNBT_big(PyObject* self, PyObject* args);
-__declspec(dllexport) extern "C" PyObject* py_writeNBT(PyObject* self, PyObject* args);
-__declspec(dllexport) extern "C" PyObject* py_readSubchunk(PyObject* self, PyObject* args);
-__declspec(dllexport) extern "C" PyObject* py_writeSubchunk(PyObject* self, PyObject* args);
+PyObject _declspec(dllexport) *py_listkeys(PyObject* self, PyObject* args);
+PyObject _declspec(dllexport) *py_loadbinary(PyObject* self, PyObject* args);
+PyObject _declspec(dllexport) *py_writebinary(PyObject* self, PyObject* args);
+PyObject _declspec(dllexport) *py_rmkey(PyObject* self, PyObject* args);
+PyObject _declspec(dllexport) *py_readNBT(PyObject* self, PyObject* args);
+PyObject _declspec(dllexport) *py_readNBT_big(PyObject* self, PyObject* args);
+PyObject _declspec(dllexport) *py_writeNBT(PyObject* self, PyObject* args);
+PyObject _declspec(dllexport) *py_readSubchunk(PyObject* self, PyObject* args);
+PyObject _declspec(dllexport) *py_writeSubchunk(PyObject* self, PyObject* args);
 #else
 PyObject* py_listkeys(PyObject* self, PyObject* args);
 PyObject* py_loadbinary(PyObject* self, PyObject* args);
